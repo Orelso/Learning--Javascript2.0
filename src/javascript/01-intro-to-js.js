866dc3027ece60c023ac9c3eb05b5e72bffed887
ORELSO <orelso25@gmail.com>
2022-06-30 11:55:05 +0300

    updated

---
+++ src/javascript/01-intro-to-js.js
@@ -0,0 +1,29 @@
+console.log("Hello World")
+/*----------------------------------------------------------------------------------- */
+
+// Single-line comments
+/* Multi-line comments */
+/*----------------------------------------------------------------------------------- */
+
+console.log(24) // Number
+console.log("twentyfour") // String
+let approved = true // Boolean
+let lastname = null // Null
+let firstName = undefined // Undefined
+/*----------------------------------------------------------------------------------- */
+
+console.log(2 + 2); // 4
+console.log(2 - 2); // 0
+console.log(2 * 2); // 4
+console.log(2 / 2); // 1
+console.log(3 / 2); // 1.5
+
+console.log(Math.floor(Math.random() * 10)) // random number no decimals
+/*----------------------------------------------------------------------------------- */
+
+console.log("Hello".length)
+console.log('hello'.toUpperCase())
+/*----------------------------------------------------------------------------------- */
+
+console.log("Hello" + ' ' + "young world") // Hello young world
+console.log("Hello " + "young world") // Hello young world
