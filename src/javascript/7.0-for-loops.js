25e2f5afa34bd6fa1369172c9e099cdac6022a44
Orelso Vitam <orelsovitam@Orelsos-Mac-Studio.local>
2022-07-07 13:49:24 +0300

    updated

--- src/javascript/7.0-for-loops.js
+++ src/javascript/7.0-for-loops.js
@@ -19,11 +19,29 @@ for (let counter = 0; counter < 4; counter++) {
 
 for ( let counter = 5; counter < 11; counter++) {
     console.log(counter)
-    }
+}
 
-    for ( let number = 5; number < 100; number++ ) {
-        console.log(number)
-      }
+for ( let number = 5; number < 100; number++ ) {
+    console.log(number)
+}
       
+
+/*-----------------------------------------------------------------------------------*/
+// Looping in Reverse
+// What if we want the for loop to log 3, 2, 1, and then 0? With simple modifications to the expressions, we can make our loop run backward!
+
+// To run a backward for loop, we must:
+
+// Set the iterator variable to the highest desired value in the initialization expression.
+// Set the stopping condition for when the iterator variable is less than the desired amount.
+// The iterator should decrease in intervals after each iteration.
+
+for (let counter = 3; counter >= 0; counter--) {
+  console.log(counter);
+}
+
+for (let number = 100; number >= 0; number--) {
+  console.log(number);
+}
 
     
